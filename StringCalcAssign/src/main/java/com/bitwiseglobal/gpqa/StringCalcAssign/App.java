e57cd6f7828638a86b95dacb81b6d73fd44aae58
saurabhk <saurabhk@U3-DPE-CPB-0094.bitwiseglobal.net>
2018-01-30 17:40:52 +0530

    initial commit

---
+++ StringCalcAssign/src/main/java/com/bitwiseglobal/gpqa/StringCalcAssign/App.java
@@ -0,0 +1,13 @@
+package com.bitwiseglobal.gpqa.StringCalcAssign;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
