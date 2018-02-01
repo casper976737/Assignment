f17dc43317e219dcae7a424ff79cf1403ec320a6
saurabhk <saurabhk@U3-DPE-CPB-0094.bitwiseglobal.net>
2018-02-01 20:00:26 +0530

    commit for Q5

---
+++ Overloader/src/MethodOverloading.java
@@ -0,0 +1,13 @@
+
+
+public class MethodOverloading {
+
+	public static void main(String[] args) {
+		
+		Overloader overload=new Overloader();
+		overload.printInput(10);
+		overload.printInput(10,20);
+		overload.printInput('a');
+	}
+
+}
---
+++ Overloader/src/Overloader.java
@@ -0,0 +1,15 @@
+
+public class Overloader {
+	void printInput(int a){
+		System.out.println(a);
+	}
+	
+	void printInput(int a,int b){
+		System.out.println(a+" "+b);
+	}
+	
+	void printInput(char a){
+		System.out.println("97");
+	}
+
+}
